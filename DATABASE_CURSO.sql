c47a171f62de5e7ff7059ca9932dc498d2564e4b
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-10-20 16:39:21 -0300

    Maneiras de realizar SELECT

--- DATABASE_CURSO.sql
+++ DATABASE_CURSO.sql
@@ -9,6 +9,7 @@ DECIMAL -> NUMEROS QUEBRADOS (INTEIRO, CASAS DECIMAIS)
 ENUM -> VALIDAS SE O INSERT CONTEM AS OPÇÕES DENTRO DOS PARENTESES ("V", "F")
 TIMESTAMP -> CARIMBO DE Tempo
 */
+
 CREATE TABLE tb_funcionarios(
 	id INT,
     nome VARCHAR(100),
@@ -19,14 +20,14 @@ CREATE TABLE tb_funcionarios(
 );
 /*COMANDO INSERT */
 INSERT INTO tb_funcionarios VALUES(1,"Gustavo","4999.00","2016-01-01", "M", NULL);
+
 INSERT INTO tb_funcionarios(id, nome, salario, admissao, sexo) VALUES(2,"Gustavo2","8999.00","2022-10-01", "M");
+
 INSERT INTO tb_funcionarios(nome, salario, admissao, sexo) VALUES("Gustavo1","8999.00","2022-10-01", "M"),("Gustavo2","5400.00","2022-10-01", "M"),("Gustavo3","8000.00","2022-10-01", "M");
 
 /* DESCREVE A TABELA FUNCIONARIOS ("FIELDS, TYPE, NULL, KEY, DEFAULT, EXTRA")*/
 DESCRIBE tb_funcionarios; 
 
-/*CONSULTANDO TODOS OS USUÁRIOS CADASTRADOS*/
-SELECT * FROM tb_funcionarios;
 
 
 /*ATUALIZA O SALARIO EM 20%(SALARIO = SALARIO * 1.2) DO USUARIO DA TABELA tb_funcionarios QUE POSSUI O id = 2*/
@@ -60,40 +61,8 @@ CAMPOS NÃO EXISTANTES COMO salario, admissao e cadastro UTILIZAMOS FUNCOES OU D
 INSERT into tb_funcionarios 
 SELECT id,nome, 950.00, CURRENT_DATE(), sexo, NULL FROM tb_pessoa;
 
-
-/*CONTA O TOTAL DE LINHAS DA TABELA tb_funcionarios*/
-SELECT COUNT(*) AS total FROM tb_funcionarios;
-
-
-/*SELECIONA O salario COMO atual E salario O SALARIO COM AUMENTO DE 20%*/
-SELECT nome,salario AS atual,
-salario * 1.2 AS "salario com aumento de 20%"
-FROM tb_funcionarios;
-
-
-/*SELECIONA O salario COMO atual E SIMULA O salario COM AUMENTO DE 20%, FORMATANDO PARA DECIMAL*/
-SELECT nome,salario AS atual,
-CONVERT(salario * 1.2, DEC(10,2)) AS "salario com aumento de 20%"
-FROM tb_funcionarios
-
-/*COMANDO SELECT */
-/*SELECIONA TODOS OS DADOS DA TABELA tb_funcionarios QUANDO O sexo FOR = "F"*/
-SELECT * FROM tb_funcionarios WHERE sexo = "F";
-/*SELECIONA TODOS OS DADOS DA TABELA tb_funcionarios QUANDO O salario FOR DIFERENTE DE 950.00 */
-SELECT * FROM tb_funcionarios WHERE salario <> 950.00;
-/* SELECIONA TODOS OS DADOS DA TABELA tb_funcionarios QUANDO O sexo FOR = "M" E O salario MAIOR QUE 9000.00 */
-SELECT * FROM tb_funcionarios WHERE sexo ="M" AND salario > 9000.00;
-/*SELECIONA TODOS OS DADOS DA TABELA tb_funcionarios QUANDO O salario FOR DIFERENTE DE 950.00 OU O sexo SEJA = "F" */
-SELECT * FROM tb_funcionarios WHERE salario <> 950.00 OR sexo ="F";
 /*ATUALIZA A TABELA tb_funcionarios AUMENTANDO O salario EM 20% QUANDO O sexo FOR = "F"*/
 UPDATE tb_funcionarios SET salario = salario * 1.2 WHERE sexo = "F";
-
-
-/*DIFERENTES MANEIRAS DE PESQUISAR A DATA NO MYSQL*/
-SELECT * FROM tb_funcionarios WHERE cadastro >= '2022-10-20 12:42:39';
-SELECT * FROM tb_funcionarios WHERE cadastro >= '2022/10/20 12:42:39';
-SELECT * FROM tb_funcionarios WHERE cadastro >= '2022.10.20 12:42:39';
-SELECT * FROM tb_funcionarios WHERE cadastro >= '20221020124239';
 
 /*ATUALIZE A admissao DOS FUNCIONARIOS PARA A DATA ATUAL QUANDO O id FOR = 2*/
 UPDATE tb_funcionarios SET admissao = CURRENT_DATE() WHERE id = 2 ;
@@ -101,44 +70,4 @@ UPDATE tb_funcionarios SET admissao = CURRENT_DATE() WHERE id = 2 ;
 /*ATUALIZE A admissao DOS FUNCIONARIOS PARA A 60 DIAS A FRENTE QUANDO O id FOR = 2*/
 UPDATE tb_funcionarios SET admissao = DATE_ADD(CURRENT_DATE(), INTERVAL 60  DAY) WHERE id = 2 ;
 
-/*VERIFICA A DIFERENÇA ENTRE A admissao E A DATA ATUAL QUANDO O id FOR = 2*/
-SELECT DATEDIFF(admissao, CURRENT_DATE()) AS "DIFERENÇA DE DIAS" FROM tb_funcionarios WHERE id = 2;
 
-/*SELECIONA OS FUNCIONARIOS ADMITIDOS NO MES 10*/
-SELECT * FROM tb_funcionarios WHERE MONTH(admissao) = 10;
-
-
-
-
-/*COMANDO LIKE */
-
-/*nome QUE CONTEM E EM QUALQUER POSICAO*/
-SELECT * FROM tb_funcionarios WHERE nome LIKE '%E%';
-
-/*nome QUE COMECE COM G*/
-SELECT * FROM tb_funcionarios WHERE nome LIKE 'G%';
-
-/*nome QUE TERMINE COM A*/
-SELECT * FROM tb_funcionarios WHERE nome LIKE '%A';
-
-/*nome QUE A TERCEIRA LETRA SEJA G */
-SELECT * FROM tb_funcionarios WHERE nome LIKE '__G%';
-
-/*nome QUE NÃO COMECE COM G*/
-SELECT * FROM tb_funcionarios WHERE nome NOT LIKE 'G%';
-
-
-/*COMANDO BETWEEN*/
-
-/*EXIBE TODOS OS FUNCIONARIOS QUE POSSUAM salario ENTRE 1000 E 3000*/
-SELECT * FROM tb_funcionarios WHERE salario BETWEEN 1000 AND 3000;
-
-/*EXIBE TODOS OS FUNCIONARIOS QUE O salario NÃO ESTEJA ENTRE 1000 E 3000*/
-SELECT * FROM tb_funcionarios WHERE salario NOT BETWEEN 1000 AND 3000;
-
-
-
-/*COMANDO SOUNDEX*/
-
-/*EXIBE TODOS OS FUNCIONARIOS QUE O nome SOE COMO Luis, EXEMPLO = "Luiz"*/
-SELECT * FROM tb_funcionarios WHERE SOUNDEX(nome) = SOUNDEX('Luis');
