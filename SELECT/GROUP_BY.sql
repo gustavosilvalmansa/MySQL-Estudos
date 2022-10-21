3305c8a3eebf0810c0418c27ec4c2c65e4e73a3e
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-10-21 15:32:06 -0300

    SELECT COM GROUP BY

--- SELECT/GROUP_BY.sql
+++ SELECT/GROUP_BY.sql
@@ -1,3 +1,27 @@
+USE curso;
+ 
+CREATE TABLE tb_pedidos(
+	idpedido INT AUTO_INCREMENT NOT NULL, 
+    idpessoa INT NOT NULL,
+    dtpedido DATETIME NOT NULL,
+    vlpedido DEC(10,2) NOT NULL,
+    CONSTRAINT PK_pedidos PRIMARY KEY (idpedido),
+    CONSTRAINT  FK_pedidos_pessoas FOREIGN KEY(idpessoa)
+		REFERENCES tb_pessoas(idpessoa)
+);
+
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(1,CURRENT_DATE(), 2000.00);
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(1,CURRENT_DATE(), 3000.00);
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(1,CURRENT_DATE(), 4000.00);
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(1,CURRENT_DATE(), 1000.00);
+
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(2,CURRENT_DATE(), 1999.99);
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(2,CURRENT_DATE(), 157.33);
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(2,CURRENT_DATE(), 123.45);
+
+INSERT INTO tb_pedidos (idpessoa,dtpedido,vlpedido)VALUES(4,CURRENT_DATE(), 40000.99);
+
+
 /*AGRUPE AS TABELAS tb_pessoas E tb_pedidos USANDO o idpessoa
 SUM-> SOMA TODOS OS PEDIDOS
 AVG-> CALCULA A MÉDIA
