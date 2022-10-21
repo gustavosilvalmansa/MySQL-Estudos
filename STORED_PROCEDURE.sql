41ca85feb58d1b5a6097c50694b4b713aa52766e
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-10-21 16:30:43 -0300

    Trabalhando com STORED PROCEDURES COM VAR, IF E ROLLBACK

--- STORED_PROCEDURE.sql
+++ STORED_PROCEDURE.sql
@@ -15,3 +15,50 @@ END $$
 DELIMITER ;
 /*CHAMA A PROCEDURE*/
 CALL sp_pessoa_save("Jotaro");
+
+
+
+/* ## SEGUNDA PROCEDURE ## */
+
+DELIMITER $$
+/*CRIA PROCEDURE sp_funcionario_save COM 3 PARAMETROS*/
+CREATE PROCEDURE sp_funcionario_save (
+	pdesnome VARCHAR(200),
+    pvsalario DECIMAL(10,2),
+    pdtadmissao DATETIME
+)
+BEGIN
+	/*DECLARA VARIAVEL vidpessoa*/
+    DECLARE vidpessoa INT;
+    
+    START TRANSACTION;
+    /*INICIA CONDICIONAL VERIFICA SE NÃO EXISTE UM USUARIO COM ESTE NOME E INSERE NO BANCO*/
+    IF NOT EXISTS(SELECT idpessoa FROM tb_pessoas WHERE desnome = pdesnome) THEN
+		INSERT INTO tb_pessoas VALUES(NULL, pdesnome, NULL);
+        /*DECLARA O vidpessoa COMO ULTIMO ID INSERIDO*/
+        SET vidpessoa = LAST_INSERT_ID();
+	ELSE
+    /*SE JÁ EXISTIR UM USUARIO ELE RETORNA A MENSAGEM DE USER CADASTRADO*/
+		SELECT   'Usuario Já Cadastrado!' AS 'resultado';
+        ROLLBACK;
+        /*FIM DO IF*/
+	END IF;
+        /*INICIA CONDICIONAL VERIFICA SE NÃO EXISTE UM USUARIO COM ESTE ID E INSERE NO BANCO*/
+    IF NOT EXISTS(SELECT idpessoa FROM tb_funcionarios WHERE idpessoa = vidpessoa) THEN
+		INSERT INTO tb_funcionarios VALUES(NULL, vidpessoa, pvsalario, pdtadmissao);
+	ELSE
+        /*SE JÁ EXISTIR UM USUARIO ELE RETORNA A MENSAGEM DE USER CADASTRADO*/
+		SELECT   'Usuario Cadastrado!' AS 'resultado';
+        /*DESFAZ AS AÇÕES ANTERIORES*/
+        ROLLBACK;
+	END IF;  
+    /*SALVA AS AÇÕES REALIZADAS PELA SP*/
+    COMMIT;
+    /*RETORNA PARA O USER*/
+    SELECT 'Dados cadastrados' AS 'resultado';
+    
+END $$
+
+DELIMITER ;
+/*CHAMA PROCEDURE*/
+CALL sp_funcionario_save('Bradock2', 5000.00, CURRENT_DATE());
